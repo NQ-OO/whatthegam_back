594ec8f4f21aaae3f6c2712899d3e06075311dee
toto9602 <toto9602@naver.com>
2022-01-12 21:20:00 +0900

    [fix] fix text CRUD errors

--- texts/views.py
+++ texts/views.py
@@ -9,7 +9,7 @@ from .serializers import *
 from whatthegam.models import School
 
 class TextListAPIView(APIView):
-
+    serializer_class = TextCreateSerializer
     def get(self, request, place_pk):
         school = School.objects.get(pk=place_pk)
         texts = Text.objects.filter(written_place=school)
@@ -18,32 +18,28 @@ class TextListAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, place_pk):
-        school = School.objects.get(id=place_pk)
+        school = School.objects.get(pk=place_pk)
         if school:
             serializer = TextCreateSerializer(data=request.data)
-
             if serializer.is_valid():
-                serializer.save(author=request.user, written_school=school)
+                serializer.save(author=request.user, written_place=school)
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class TextDetailAPIView(APIView):
 
-    def get_object(self, text_pk):
-        text = get_object_or_404(Text, pk=text_pk)
-        return text
-
     def put(self, request, place_pk, text_pk):
         school = School.objects.get(id=place_pk)
-        text = self.get_object(pk=text_pk)
+        text = get_object_or_404(Text, id=text_pk)
         serializer = TextCreateSerializer(text, data=request.data)
         if serializer.is_valid():
             serializer.save(author=request.user, written_school=school)
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializers.errors, status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, text_pk):
-        text = self.get_object(pk=text_pk)
+    def delete(self, request, place_pk, text_pk):
+        text = Text.objects.get(id=text_pk)
         text.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
