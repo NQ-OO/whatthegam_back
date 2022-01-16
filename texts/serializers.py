ded24ab8babaa6f165847d11d7b19e324127aeac
toto9602 <toto9602@naver.com>
2022-01-16 11:17:40 +0900

    [fix] : /name 응답에 장소 정보 추가

--- texts/serializers.py
+++ texts/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
+from users.models import CustomUser
 from .models import Text
 from whatthegam.models import Place
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
 
 
 class PlaceSerializer(serializers.ModelSerializer):
@@ -11,21 +18,67 @@ class PlaceSerializer(serializers.ModelSerializer):
 
 class TextSerializer(serializers.ModelSerializer):
     author_year = serializers.SerializerMethodField(method_name='get_author_year')
+    create_dt = serializers.SerializerMethodField(method_name='get_create_dt')
 
     class Meta:
         model = Text
-        fields = ('id', 'content', 'created_dt', 'x_axis', 'y_axis', 'author', 'written_place', 'author_year')
+        fields = ('id', 'content', 'author', 'create_dt', 'written_place', 'x_axis', 'y_axis', 'spin_rate', \
+            'author_year', 'created_dt')
     
     def get_author_year(self, obj):
         return obj.author.year
+    
+    def get_create_dt(self, obj):
+        return obj.created_dt.strftime("%Y-%m-%d %H:%M:%S")
 
 
 class TextCreateSerializer(serializers.ModelSerializer):
     author_year = serializers.SerializerMethodField(method_name='get_author_year')
+    create_dt = serializers.SerializerMethodField(method_name='get_create_dt')
 
     class Meta:
         model = Text
-        fields = ('id', 'author', 'content', 'created_dt', 'x_axis', 'y_axis', 'spin_rate', 'author_year')
+        fields = ('id', 'content', 'author', 'create_dt', 'written_place', 'x_axis', 'y_axis', 'spin_rate', 'author_year')
 
     def get_author_year(self, obj):
+        print(obj.author)
         return obj.author.year
+
+    def get_create_dt(self, obj):
+        return obj.created_dt.strftime("%Y-%m-%d %H:%M:%S")
+
+        
+    # def update(self, instance, validated_data):
+    #     print(validated_data)
+    #     if validated_data['is_private'] == False:
+    #         instance = self.Meta.model(**validated_data)
+    #         if instance:
+    #             instance.save()
+    #         return instance
+    #     else:
+    #         if validated_data['users_opened']:
+    #             print(validated_data)
+    #             users = validated_data.pop('users_opened')
+    #             print(validated_data)
+    #             print(users)
+    #             user_ids = [user.id for user in users]
+    #             print(type(users[0]))
+    #             instance = self.Meta.model(**validated_data)
+    #             if instance:
+    #                 instance.save()
+    #                 for user in users:
+    #                     instance.users_opened.add(user)
+    #             return instance
+
+    # def create(self, validated_data):
+    #     print("debug#3")
+    #     print(validated_data)
+    #     user = validated_data.pop('users_opened')
+    #     print(user)
+    #     text = Text.objects.create(**validated_data)
+    #     print(text)
+    #     print(text.users_opened)
+    #     text.users_opened.add(user)
+    #     print(text.users_opened)
+
+    #     return text
--- texts/views.py
+++ texts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from django.contrib.auth.models import User
+from users.models import CustomUser
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -14,31 +14,34 @@ class TextCountAPIView(APIView):
         place = Place.objects.get(map_id=map_id)
         text_count = Text.objects.filter(written_place=place).count()
         context = {}
+        if text_count == 0:
+            context['msg'] = '낙서가 없는 장소입니다!'
+        else:
+            context['msg'] = f'낙서가 {text_count}개 있는 장소입니다!'
         context['text_count'] = text_count
+        context['map_id'] = map_id
+        context['place'] = place.name
+        
         return Response(context, status=status.HTTP_200_OK)
 
 
 class TextListAPIView(APIView):
 
-    def get(self, request, map_id):
-        try:
-            place = Place.objects.get(map_id=map_id)
-        except:
-            place_serializer = PlaceSerializer(data=request.data)
-            if place_serializer.is_valid():
-                place_serializer.save()
-                
+    def get(self, request, map_id):                
         place = Place.objects.get(map_id=map_id)
         texts = Text.objects.filter(written_place=place)
+        context = {}
         if texts:
             serializer = TextSerializer(texts, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            context['data'] = serializer.data
+            context['text_count'] = texts.count()
+            return Response(context, status=status.HTTP_200_OK)
         else:
-            msg = {"msg": "낙서가 없는 보드입니다!"}
-            return Response(msg, status=status.HTTP_204_NO_CONTENT)
+            context['text_count'] = texts.count()
+            context['data'] = []
+            return Response(context, status=status.HTTP_204_NO_CONTENT)
 
     def post(self, request, map_id):
-        print(request.data)
         place = Place.objects.get(map_id=map_id)
         if place:
             serializer = TextCreateSerializer(data=request.data)
@@ -48,28 +51,59 @@ class TextListAPIView(APIView):
                                 spin_rate=round(random.uniform(0, 360), 2),
                                 x_axis=round(random.uniform(0,100), 2),
                                 y_axis=round(random.uniform(0,100), 2))
-            
-        # all_texts = Text.objects.filter(written_place=place)
-        # if all_texts:
-        #     text_seri = TextSerializer(all_texts, many=True)
+
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
-
+    
 
 class TextDetailAPIView(APIView):
+
 
     def put(self, request, map_id, text_pk):
         place = Place.objects.get(map_id=map_id)
         text = get_object_or_404(Text, id=text_pk)
+
         serializer = TextCreateSerializer(text, data=request.data)
         if serializer.is_valid():
-            serializer.save(author=request.user, written_place=place)
+            serializer.save(author=request.user)
             return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializers.errors, status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, map_id, text_pk):
         text = Text.objects.get(id=text_pk)
         text.delete()
-        msg = {'msg':"낙서가 삭제되었습니다!"}
+        msg = {'msg':"낙서가 지워졌습니다!"}
         return Response(msg, status=status.HTTP_204_NO_CONTENT)
 
+
+
+
+
+# def put(self, request, map_id, text_pk):
+#         print(request.user)
+#         place = Place.objects.get(map_id=map_id)
+#         text = get_object_or_404(Text, id=text_pk)
+#         if (request.data['is_private'] and request.data['users_opened']):
+#             print("debug#0")
+#             users = []
+#             print(request.data['users_opened'])
+#             for user in request.data['users_opened']:
+#                 print(CustomUser.objects.get(username=user))
+#                 users.append(CustomUser.objects.get(username=user).id)
+#             print(users)
+#         print("debug#1")
+#         # request.data.pop('users_opened')
+#         # request.data['users_opened'] = users
+#         request.data['author'] = request.user
+#         print(request.data)
+#         request.data['users_opened'] = users
+#         serializer = TextCreateSerializer(text, data=request.data)
+#         print(serializer.is_valid())
+#         print(serializer.errors)
+#         if serializer.is_valid():
+#             serializer.save()
+#             print(serializer.data)
+#             serializer.data['author_year'] = request.user.year
+#             print(serializer.data)
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
